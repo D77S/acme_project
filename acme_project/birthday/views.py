28704ae622e326ef39d2bd77cdfcb5c9a716e0d4
Дмитрий <d77s@inbox.ru>
2023-09-01 20:16:37 +0300

    Next

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -8,24 +8,37 @@ from .utils import calculate_birthday_countdown
 def birthday(request, pk=None):
     if pk is not None:
         # Получаем объект модели или выбрасываем 404 ошибку.
+        print('\npk не является None. А является', pk)
         instance = get_object_or_404(Birthday, pk=pk)
-    # Если в запросе не указан pk
+        print('\nИзвлекли из базы объект по ключу pk и записали его в instance. Он такой: ', instance)
+        # Если в запросе не указан pk
     # (если получен запрос к странице создания записи):
     else:
         # Связывать форму с объектом не нужно, установим значение None.
+        print('\npk является None.')
         instance = None
-    # Передаём в форму либо данные из запроса, либо None.
+        print('\nУстановим instance в None. Он такой: ', instance)
+        # Передаём в форму либо данные из запроса, либо None.
     # В случае редактирования прикрепляем объект модели.
+    print('\nrequest такой:', request)
+    print('\nrequest.POST такой: ', request.POST)
+    print('\nrequest.POST or None такой: ', (request.POST or None))
     form = BirthdayForm(request.POST or None, instance=instance)
+    print('\nВ form попало такое: ', form)
     # Остальной код без изменений.
     context = {'form': form}
     # Сохраняем данные, полученные из формы, и отправляем ответ:
+    print('\nform.is_valid такой: ', form.is_valid)
     if form.is_valid():
+        print('\nСохраняем форму в базу')
         form.save()
         birthday_countdown = calculate_birthday_countdown(
             form.cleaned_data['birthday']
         )
         context.update({'birthday_countdown': birthday_countdown})
+    else:
+        print('\nНе сохраняем форму в базу.')
+    print('\nДошли до рендера. Context такой: ', context)
     return render(request, 'birthday/birthday.html', context)
 
 
