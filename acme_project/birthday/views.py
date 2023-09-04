c8b913eefd20979e458288d4d57e804cbbea5f84
Дмитрий <d77s@inbox.ru>
2023-09-04 20:53:49 +0300

    Next

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
+
+
+class BirthdayMixin:
+    model = Birthday
+    form_class = BirthdayForm
+    template_name = 'birthday/birthday.html'
+    success_url = reverse_lazy('birthday:list')
 
 
 # Наследуем класс от встроенного ListView.
@@ -18,17 +25,20 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayCreateView(CreateView):
-    # Указываем модель, с которой работает CBV...
+# Добавляем миксин первым по списку родительских классов.
+class BirthdayCreateView(BirthdayMixin, CreateView):
+    # Не нужно описывать атрибуты: все они унаследованы от BirthdayMixin.
+    pass
+
+
+class BirthdayUpdateView(BirthdayMixin, UpdateView):
+    # И здесь все атрибуты наследуются от BirthdayMixin.
+    pass
+
+
+class BirthdayDeleteView(DeleteView):
     model = Birthday
-    # Этот класс сам может создать форму на основе модели!
-    # Нет необходимости отдельно создавать форму через ModelForm.
-    # Указываем имя формы:
-    form_class = BirthdayForm
-    # Явным образом указываем шаблон:
     template_name = 'birthday/birthday.html'
-    # Указываем namespace:name страницы, куда будет перенаправлен пользователь
-    # после создания объекта:
     success_url = reverse_lazy('birthday:list')
 
 
@@ -37,7 +47,7 @@ def birthday(request, pk=None):
         # Получаем объект модели или выбрасываем 404 ошибку.
         print('\npk не является None. А является', pk)
         instance = get_object_or_404(Birthday, pk=pk)
-        print('\nИзвлекли из базы объект по ключу pk и записали его в instance. Он такой: ', instance)
+        print('\ninstance такой: ', instance)
         # Если в запросе не указан pk
     # (если получен запрос к странице создания записи):
     else:
@@ -61,7 +71,7 @@ def birthday(request, pk=None):
     # Сохраняем данные, полученные из формы, и отправляем ответ:
     print('\nform.is_valid такой: ', form.is_valid())
     if not (request.POST) and (not (form.is_valid())):
-        print('\nФорма с GET-запросом. Вроде не содержит ничего невалидного, но почему-то признана невалидной.')
+        print('\nФорма с GET-запросом, признана невалидной.')
     if form.is_valid():
         print('\nСохраняем форму в базу')
         form.save()
