810335bae648a1330288a2451068a729dbacfe1e
Dmitry <d77s@inbox.ru>
2023-09-04 18:15:02 +0300

    Next

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
+from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
+
+
+# Наследуем класс от встроенного ListView.
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10
+
+
+class BirthdayCreateView(CreateView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # Этот класс сам может создать форму на основе модели!
+    # Нет необходимости отдельно создавать форму через ModelForm.
+    # Указываем имя формы:
+    form_class = BirthdayForm
+    # Явным образом указываем шаблон:
+    template_name = 'birthday/birthday.html'
+    # Указываем namespace:name страницы, куда будет перенаправлен пользователь
+    # после создания объекта:
+    success_url = reverse_lazy('birthday:list')
 
 
 def birthday(request, pk=None):
