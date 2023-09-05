379b88e412d23819f3efdcde26ca00729eef567b
Dmitry <d77s@inbox.ru>
2023-09-05 18:00:20 +0300

    Next

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,11 +1,12 @@
 # from django.shortcuts import get_object_or_404, redirect, render
 # from django.core.paginator import Paginator
-from django.views.generic import CreateView, DeleteView, ListView, UpdateView
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView)
 from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
 from .models import Birthday
-# from .utils import calculate_birthday_countdown
+from .utils import calculate_birthday_countdown
 
 
 class BirthdayMixin:
@@ -37,6 +38,21 @@ class BirthdayUpdateView(BirthdayMixin, BirthdayFormMixin, UpdateView):
 
 class BirthdayDeleteView(BirthdayMixin, DeleteView):
     pass
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday  # type: ignore
+        )
+        # Возвращаем словарь контекста.
+        return context
 
 
 # def birthday(request, pk=None):
